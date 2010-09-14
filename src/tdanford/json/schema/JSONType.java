9676d063f50bf9d87ce01a3ee18538318589d56b
Timothy Danford <tdanford@gmail.com>
2010-09-14 15:32:54 -0400

    Added the All and Empty internal types.
    All "contains" every non-null value.
    Empty "contains" no values.

--- src/tdanford/json/schema/JSONType.java
+++ src/tdanford/json/schema/JSONType.java
@@ -3,6 +3,18 @@ package tdanford.json.schema;
 public interface JSONType { 
 
 	public boolean contains(Object obj);
+	
+	public static class Everything implements JSONType { 
+		public boolean contains(Object obj) { 
+			return obj != null;
+		}
+	}
+	
+	public static class Empty implements JSONType { 
+		public boolean contains(Object obj) { 
+			return false;
+		}
+	}
 
 	public static class String implements JSONType { 
 		public boolean contains(Object obj) { 
