8f5a53cca4b9fcb43994d2c3bc2464853607cd57
Timothy Danford <tdanford@gmail.com>
2010-09-15 08:07:47 -0400

    Added a main() method to SchemaValidator.

--- src/tdanford/json/schema/SchemaValidator.java
+++ src/tdanford/json/schema/SchemaValidator.java
@@ -7,6 +7,22 @@ import org.json.JSONObject;
 
 public class SchemaValidator { 
 	
+	public static void main(String[] args) { 
+		if(args.length != 2) { 
+			System.err.println("USAGE: SchemaValidator <schema-file> <json-value-file>");
+			System.exit();
+		}
+		try { 
+			File schemaFile = new File(args[0]);
+			File valueFile = new File(args[1]);
+			JSONType type = new JSONFileType(new SchemaEnv(), schemaFile);
+			JSONObject value = new JSONObject(new FileReader(valueFile));
+			System.out.println(String.valueOf(type.contains(value)));
+		} catch(Throwable t) {
+			System.err.println(String.format("ERROR: %s", t.getMessage()));
+		}
+	}
+
 	private SchemaEnv env;
 	
 	public SchemaValidator() { 
